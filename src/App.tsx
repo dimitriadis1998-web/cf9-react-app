afddc103e6f30e8e5e75611dc4d6a1345528ae24
ΤοΌνομάΣου <email@.com>
2026-07-05 11:54:45 +0300

    feat : add Header , Footer and Layout Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,104 @@
-import FunctionalComponent from "./components/LesssonTwo/FunctionalComponent.tsx";
-import ClassComponent from "./components/LesssonTwo/ClassComponent.tsx";
-import ArrowFunctionalComponent from "./components/LesssonTwo/ArrowFunctionalComponent.tsx";
+// import FunctionalComponent from "./components/LesssonTwo/FunctionalComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./components/LessonThree/ArrowFunctionalComponentWithProps.tsx";
+// import ClassComponent from "./components/LesssonTwo/ClassComponent.tsx";
+// import ArrowFunctionalComponent from "./components/LesssonTwo/ArrowFunctionalComponent.tsx";
+import Layout from "./components/Layout.tsx";
+
+
+// type PropsA = {
+//   title :string,
+//   description : string,
+// }
+//
+// type PropsB = {
+//   title :string,
+//   description : string,
+// }
+//
+//
+//
+// // interface PropsTwo {
+// //   title: string,
+// //   description : string,
+// // }
+//
+//
+// // Union
+// type Status = "Error" | "Info" | "Warning"
+//
+// // Tuples
+//  type Coordinates = [number , number];
+//
+// // Functions
+//
+// type ClickHandler = (event :MouseEvent) => void;
+//
+// // Intersection
+//
+// type Props = PropsA & PropsB;
+//
+//
+//
+// interface PropsTwo {
+//   title: string,
+//   description : string,
+// }
+//
+// interface PropsTwo {
+//   price: number,
+//   sort : number,
+// }
+//
+// //PropsTwo = {title, description, price, sort}
+//
+// interface PropsThree extends PropsTwo {
+//   image : string,
+// }
+//
+// //PropsThree = {title, description, price, sort, image}
+
+
+
+
 
 function App() {
 
-
   return (
     <>
-     <FunctionalComponent/>
-      <ClassComponent/>
-      <ArrowFunctionalComponent/>
+     {/*<FunctionalComponent/>*/}
+     {/*<FunctionalComponent/>*/}
+     {/*<FunctionalComponent/>*/}
+     {/*/!* <ClassComponent/>*!/*/}
+     {/*/!* <ArrowFunctionalComponent/>*!/*/}
+
+
+      {/*<ArrowFunctionalComponentWithProps*/}
+      {/*    title="Heading 1"/>*/}
+      {/*<ArrowFunctionalComponentWithProps*/}
+      {/*    title="Second Title"*/}
+      {/*    description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum cupiditate dolorem earum inventore labore nemo, unde voluptate. Explicabo, voluptatum!" />*/}
+
+
+
+        <Layout>
+            <ArrowFunctionalComponentWithProps
+                title="Second Title"
+                description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur cum cupiditate dolorem
+                earum inventore labore nemo, unde voluptate. Explicabo, voluptatum!" />
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur culpa debitis dolore ea excepturi illo illum itaque laborum molestias,
+                necessitatibus nisi obcaecati perspiciatis quos reprehenderit sunt, tempora tenetur totam ut?</p>
+
+        </Layout>
+
+
+
+
+
     </>
+
+
+
+
   )
 }
 

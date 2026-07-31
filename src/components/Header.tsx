80cd6a4c859d4f1b5437cb259b76fde9a2ff99b3
ΤοΌνομάΣου <email@.com>
2026-07-31 20:05:52 +0300

    feat(auth): add AuthButton on Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import {Link} from "react-router";
+import {AuthButton} from "@/components/AuthButton.tsx";
 
 const Header = () => {
     return (
@@ -6,19 +7,17 @@ const Header = () => {
             <header className="bg-cf-dark-red fixed w-full z-50">
                 <div className="container mx-auto px-4 flex items-center justify-between">
                     <Link to="/">
-                        <img
-                            src="https://codingfactory.aueb.gr/sites/all/themes/cf_theme/logo.png"
-                            alt="CF logo"
-                            className="my-4 h-16" />
+                        <img src="https://codingfactory.aueb.gr/sites/all/themes/cf_theme/logo.png"
+                             alt="CF Logo" className="my-4 h-16"/>
                     </Link>
-                <nav className="flex gap-4 text-white font-medium">
-                    <Link to="/"> Home </Link>
-                    <Link to="/products"> Products </Link>
-                </nav>
+                    <div className="flex items-center gap-4 text-white font-medium">
+                        <Link to="/">Home</Link>
+                        <Link to="/products">Products</Link>
+                        <AuthButton/>
+                    </div>
                 </div>
             </header>
         </>
-    );
-};
-
+    )
+}
 export default Header;
